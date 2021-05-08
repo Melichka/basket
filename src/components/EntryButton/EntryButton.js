bf468c0550e53ce02bb7183d90eefaa3330f262f
Melichka <melichkaowen1@gmail.com>
2021-05-08 04:10:45 +0300

    extract entry button into components

---
+++ src/components/EntryButton/EntryButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EntryButton.styles.css";
+
+function EntryButton() {
+  return (
+    <button className="button__entry">
+      <span className="button__entry-text">Войти</span>
+    </button>
+  );
+}
+
+export default EntryButton;
