5185d98850368197639776775baf842e78909aa5
Melichka <melichkaowen1@gmail.com>
2021-05-08 04:36:43 +0300

    add switch for entry button

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -7,7 +7,7 @@ function Buttons() {
   return (
     <div className="button__container">
       <ForgetButton />
-      <EntryButton text="Войти" />
+      <EntryButton before="Войти" after="Viyti" />
     </div>
   );
 }
--- src/components/EntryButton/EntryButton.js
+++ src/components/EntryButton/EntryButton.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EntryButton.styles.css";
 
 function EntryButton(props) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function onButtonClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <button className="button__entry">
-      <span className="button__entry-text">{props.text}</span>
+    <button onClick={onButtonClick} className="button__entry">
+      {isOpen && <span className="button__entry-text">{props.before}</span>}
+      {!isOpen && <span className="button__entry-text">{props.after}</span>}
     </button>
   );
 }
