98966c64eab26a651f3493331aacc0f933140926
Melichka <melichkaowen1@gmail.com>
2021-05-09 01:39:56 +0300

    change code to component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import React from "react";
-import Buttons from "./components/Buttons/Buttons";
+import FormContainer from "./components/FormContainer/FormContainer";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="form__container">
-      <div className="title__container">
-        <p className="title">Авторизация</p>
-      </div>
-      <div className="field__container">
-        <div className="field__login">
-          <input type="text" name="login" placeholder="Логин" required />
-          <input
-            type="password"
-            name="password"
-            placeholder="Пароль"
-            required
-          />
-        </div>
-        <Buttons />
-      </div>
-    </div>
-  );
+  return <FormContainer />;
 }
 
 export default App;
