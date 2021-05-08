b1da12f41837ab4255680d677aaca829bbb24f71
Melichka <melichkaowen1@gmail.com>
2021-05-08 04:09:48 +0300

    extract forget button into component

---
+++ src/components/ForgetButton/ForgetButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ForgetButton.styles.css";
+
+function ForgetButton() {
+  return (
+    <a className="button__forget" href="#">
+      Забыли пароль?
+    </a>
+  );
+}
+
+export default ForgetButton;
