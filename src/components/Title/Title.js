6ca1807b4a22a60d2f655af0a5e08d595116af7e
Melichka <melichkaowen1@gmail.com>
2021-05-09 01:34:31 +0300

    extract title into component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="title__container">
+      <p className="title">Авторизация</p>
+    </div>
+  );
+}
+
+export default Title;
