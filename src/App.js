fde6540eb6fdf83fe94b59eddc0697d2d5a531d2
Melichka <melichkaowen1@gmail.com>
2021-05-08 03:36:11 +0300

    add login-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form__container">
+      <div className="title__container">
+        <p className="title">Авторизация</p>
+      </div>
+      <div className="field__container">
+        <div className="field__login">
+          <input type="text" name="login" placeholder="Логин" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            required
+          />
+        </div>
+        <div className="button__container">
+          <a className="button__forget" href="#">
+            Забыли пароль?
+          </a>
+          <button className="button__entry">
+            <span className="button__entry-text">Войти</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
